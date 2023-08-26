c48b2f9c62d5f210f99127d97ee56428653d2f05
diegogirao <diegoramiro25@gmail.com>
2023-08-26 18:57:54 +0100

    feat: config nodemailer variables

--- src/config.js
+++ src/config.js
@@ -1,18 +1,19 @@
-const dotenv = require('dotenv');
-const path = require('path');
+const dotenv = require("dotenv")
+const path = require("path")
 
-const envPath = path.resolve(__dirname, '../.env');
+const envPath = path.resolve(__dirname, "../.env")
 
 dotenv.config({
-  path: envPath,
-});
+	path: envPath,
+})
 
 module.exports = {
-  AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY,
-  AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
-  AWS_REGION: process.env.AWS_REGION,
-  AWS_API_VERSION: process.env.AWS_API_VERSION,
-  REDIS_HOST: process.env.REDIS_HOST,
-  REDIS_PORT: process.env.REDIS_PORT,
-  EMAIL_FROM: process.env.EMAIL_FROM
-};
+	NODEMAILER_HOST: process.env.NODEMAILER_HOST,
+	NODEMAILER_PORT: process.env.NODEMAILER_PORT,
+	NODEMAILER_SECURE: process.env.NODEMAILER_SECURE,
+	NODEMAILER_USER: process.env.NODEMAILER_USER,
+	NODEMAILER_PASSWORD: process.env.NODEMAILER_PASSWORD,
+	REDIS_HOST: process.env.REDIS_HOST,
+	REDIS_PORT: process.env.REDIS_PORT,
+	EMAIL_FROM: process.env.EMAIL_FROM,
+}
