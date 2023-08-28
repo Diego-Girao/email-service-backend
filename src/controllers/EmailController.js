d073b5ae4c428b99695c3bb2d2292e363c3ea94e
diegogirao <diegoramiro25@gmail.com>
2023-08-28 18:44:53 +0100

    feat: created successful email controller component

--- src/controllers/EmailController.js
+++ src/controllers/EmailController.js
@@ -1,30 +1,26 @@
-const MailQueue = require("../queue/MailQueue");
+const MailQueue = require("../queue/MailQueue")
+const link = "https://github.com/Diego-Girao/email-service-frontend"
 
-async function sendEmail(request, reply){
-    const {
-        email,
-        firstName,
-        lastName
-    } = request.body
+async function sendEmail(request, reply) {
+	const { email, firstName, lastName } = request.body
 
-    const template = `
-        Olá ${firstName} ${lastName}, sua assinatura foi confirmada!
-        Para acessar seus recursos exclusivos você precisa basta clicar aqui.
-    `
+	const template = `
+        Olá <strong>${firstName} ${lastName}</strong>, sua assinatura foi confirmada!
+        <br>Acesse seus recursos exclusivos, clique <a href="${link}">aqui</a>`
 
-    try {
-        await MailQueue.add({
-            to: email,
-            from: process.env.EMAIL_FROM,
-            subject: "Assinatura Confirmada",
-            text: template
-        })
-        return reply.code(200).send();
-    } catch {
-        return reply.code(500).send("Internal Server Error");
-    }
+	try {
+		await MailQueue.add({
+			to: email,
+			from: process.env.EMAIL_FROM,
+			subject: "Assinatura Confirmada",
+			html: template,
+		})
+		return reply.code(200).send()
+	} catch {
+		return reply.code(500).send("Internal Server Error")
+	}
 }
 
 module.exports = {
-    sendEmail
+	sendEmail,
 }
