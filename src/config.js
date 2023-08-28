e47dbcaf89cbf395239a49b17e316df26ea1aae3
diegogirao <diegoramiro25@gmail.com>
2023-08-28 18:43:17 +0100

    refact: improve variable names

--- src/config.js
+++ src/config.js
@@ -8,11 +8,11 @@ dotenv.config({
 })
 
 module.exports = {
-	NODEMAILER_HOST: process.env.NODEMAILER_HOST,
-	NODEMAILER_PORT: process.env.NODEMAILER_PORT,
-	NODEMAILER_SECURE: process.env.NODEMAILER_SECURE,
-	NODEMAILER_USER: process.env.NODEMAILER_USER,
-	NODEMAILER_PASSWORD: process.env.NODEMAILER_PASSWORD,
+	SMTP_HOST: process.env.SMTP_HOST,
+	SMTP_PORT: process.env.SMTP_PORT,
+	SMTP_SECURE: process.env.SMTP_SECURE,
+	SMTP_USER: process.env.SMTP_USER,
+	SMTP_PASS: process.env.SMTP_PASS,
 	REDIS_HOST: process.env.REDIS_HOST,
 	REDIS_PORT: process.env.REDIS_PORT,
 	EMAIL_FROM: process.env.EMAIL_FROM,
